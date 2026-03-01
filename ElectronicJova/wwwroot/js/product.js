22a50980867bd0f3f1f451cffd741fe220285cb9
Victor Manuel <victor.manuel.pp98@gmail.com>
2026-03-01 16:15:35 -0600

    feat: Implement initial Admin area with dashboard, product, category, and order management, including data tables and CRUD operations.

--- ElectronicJova/wwwroot/js/product.js
+++ ElectronicJova/wwwroot/js/product.js
@@ -13,20 +13,21 @@ function loadDataTable() {
     dataTable = tbl.DataTable({
         "ajax": { url: urlGetAll },
         "columns": [
-            { data: 'name', "width": "20%" },
-            { data: 'model', "width": "15%" },
-            { data: 'listPrice', "width": "10%" },
-            { data: 'brand', "width": "15%" },
+            { data: 'name', "width": "25%", "className": "ps-4" },
+            { data: 'model', "width": "15%", "className": "d-none d-lg-table-cell" },
+            { data: 'listPrice', "width": "10%", "className": "d-none d-md-table-cell" },
+            { data: 'brand', "width": "15%", "className": "d-none d-md-table-cell" },
             { data: 'category.name', "width": "15%" },
             {
                 data: 'id',
                 "render": function (data) {
-                    return `<div class="w-75 btn-group" role="group">
-                        <a href="${urlUpsert}?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
-                        <a onClick=Delete('${urlDelete}/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    return `<div class="btn-group" role="group">
+                        <a href="${urlUpsert}?id=${data}" class="btn btn-primary btn-sm mx-1 rounded"> <i class="bi bi-pencil-square"></i></a>
+                        <a onClick=Delete('${urlDelete}/${data}') class="btn btn-danger btn-sm mx-1 rounded"> <i class="bi bi-trash-fill"></i></a>
                     </div>`
                 },
-                "width": "25%"
+                "width": "20%",
+                "className": "text-end pe-4"
             }
         ]
     });
